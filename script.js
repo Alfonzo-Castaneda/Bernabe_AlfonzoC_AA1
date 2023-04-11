1e3e1b64587fbdfe50111037a9ff08f3319d1448
Alfonzo C. Bernabe <126536046+Alfonzo-Castaneda@users.noreply.github.com>
2023-04-11 10:56:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("fname").innerHTML = prompt("What is your name?");
+document.getElementById("gender").innerHTML  = prompt("What is you Gender Identity?");
+document.getElementById("username").innerHTML = prompt("Input your username");
+document.getElementById("year").innerHTML  = prompt("When were you born?");
+document.getElementById("desc").innerHTML = prompt("Describe Yourself");
+
